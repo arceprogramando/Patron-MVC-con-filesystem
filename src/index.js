6b2bc8426c1711f96441ec8568ff603c6ac09273
Felipe Arce <arceprogramando@gmail.com>
2023-05-26 23:24:16 -0300

    actualizacion estudio(sin terminar )

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-import express from "express";
+import express from 'express' //Es Modules
+import userRouter from '../routers/user.router.js'
+import petsRouter from '../routers/pets.router.js'
 
+let logged = true
 const app = express()
 
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))//Solo es necesario cuando los datos se envian por formulario
+app.use('/contenido', express.static('public'))
 
-app.get('/', (request, response) => {
-    response.send("Bienvenida");
+app.get('/', (req, res) => {
+    res.send('Bienvenida')
+})
+// Middleware
+app.use(function (req, res, next) {
+    console.log('Validando login de usuario que hizo la consulta')
+    if (logged) {
+        next()
+    } else {
+        res.send('Error no estas logeado')
+    }
+    next()
 })
 
-app.get('/users', (request, response) => {
-    response.send("Usuarios");
-})
+app.use('/users', userRouter);
+app.use('/pets', petsRouter)
 
 const PORT = 8080
 
-app.listen(PORT, () => console.log(`Se levanto el servidor por el puerto http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`Levantando el servidor en http://localhost:${PORT}`))
