7822c1966dfd6b4c6ae5f35ae53fc30b215eb977
Felipe Arce <arceprogramando@gmail.com>
2024-03-18 02:47:08 -0300

    feat(Backend): :recycle: Refactor app.js to reorganize elements without altering logic.

--- src/app.js
+++ src/app.js
@@ -1,23 +1,20 @@
 import express from 'express';
-import productsRouter from '../routers/products.routes.js'
-import cartsRouter from '../routers/carts.routes.js'
+import path from 'path';
+import productsRouter from '../routers/products.routes.js';
+import cartsRouter from '../routers/carts.routes.js';
 import __dirname from './utils.js';
-import path from 'path'
 import appRouter from './services/uploader.js';
 
 const app = express();
-
-app.use('/api', express.static(path.join(__dirname, 'public')))
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
-
-
-app.use('/api/products', productsRouter);
-app.use('/api/carts', cartsRouter)
-
-app.use('/', appRouter)
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = 8080;
+app.listen(PORT, () => console.log(`Levantando el servidor en \n "http://localhost:${PORT}/api"`));
 
-app.listen(PORT, () => console.log(`Levantando el servidor en \n "http://localhost:${PORT}/api"`))
+const viewRouter = express.static(path.join(__dirname, 'public'));
+
+app.use('/', appRouter);
+app.use('/api', viewRouter);
+app.use('/api/products', productsRouter);
+app.use('/api/carts', cartsRouter);
