9aaecc71702a86bfa1aa5dec8f558aca4c205e71
Felipe Arce <arceprogramando@gmail.com>
2024-03-18 03:28:39 -0300

    refactor(Backend): :recycle: Refactor the code with minimal alterations.

--- src/ProductManager.js
+++ src/ProductManager.js
@@ -1,103 +1,93 @@
-import { promises as fs } from 'fs';
+import fs from 'fs/promises';
 
 class ProductManager {
+  constructor(filePath) {
+    this.filePath = filePath;
+  }
 
-    constructor(filePath) {
-        this.filePath = filePath;
+  async getProducts() {
+    try {
+      const data = await fs.readFile(this.filePath, 'utf-8');
+      const products = JSON.parse(data);
+      return products;
+    } catch (error) {
+      throw new Error('Error al obtener los productos ');
     }
+  }
 
-    // Lectura
-    async getProducts() {
-
-        try {
-            const data = await fs.readFile(this.filePath, 'utf-8');
-            const products = JSON.parse(data);
-            return products;
-        } catch (error) {
-            throw new Error('Error al obtener los productos ')
-        }
+  async getProductById(pid) {
+    try {
+      const data = await fs.readFile(this.filePath, 'utf-8');
+      const products = JSON.parse(data);
+      const product = products.find((p) => p.id === Number(pid));
+      return product;
+    } catch (error) {
+      throw new Error('Error al obtener los productos.');
     }
+  }
 
-    async getProductById(pid) {
-
-        try {
-            const data = await fs.readFile(this.filePath, 'utf-8');
-            const products = JSON.parse(data);
-            const product = products.find((p) => p.id === parseInt(pid));
-            return product;
-        }
-        catch (error) {
-            throw new Error('Error al obtener los productos.')
-        }
+  async generateProductId() {
+    try {
+      const products = await this.getProducts();
+      const nextProductId = products.length > 0 ? products.length + 1 : 1;
+      return nextProductId;
+    } catch (error) {
+      throw new Error('Error al generar el ID del producto');
     }
+  }
 
-    // Escritura Product
-    async generateProductId() {
-        try {
-            const products = await this.getProducts();
-            const nextProductId = products.length > 0 ? products.length + 1 : 1;
-            return nextProductId;
-        } catch (error) {
-            throw new Error('Error al generar el ID del producto');
-        }
+  async writeProduct(product) {
+    try {
+      const nextProductId = await this.generateProductId();
+      const products = await this.getProducts();
+
+      const updatedProduct = { id: nextProductId, ...product };
+      const updatedProducts = [...products, updatedProduct];
+
+      await fs.writeFile(this.filePath, JSON.stringify(updatedProducts, null, '\t'));
+      return updatedProduct;
+    } catch (error) {
+      throw new Error('Error al escribir el producto');
     }
+  }
 
-    async writeProduct(product) {
-        try {
-            const nextProductId = await this.generateProductId();
-            const products = await this.getProducts();
+  async updateProduct(pid, updatedFields) {
+    try {
+      const products = await this.getProducts();
+      const index = products.findIndex((p) => p.id === Number(pid));
 
-            const updatedProduct = { id: nextProductId, ...product };
-            const updatedProducts = [...products, updatedProduct];
+      if (index === -1) {
+        throw new Error('El producto no existe');
+      }
 
-            await fs.writeFile(this.filePath, JSON.stringify(updatedProducts, null, '\t'));
-            return updatedProduct;
-        } catch (error) {
-            throw new Error('Error al escribir el producto');
-        }
+      const updatedProduct = {
+        ...products[index],
+        ...updatedFields,
+      };
+
+      products[index] = updatedProduct;
+      await fs.writeFile(this.filePath, JSON.stringify(products, null, '\t'));
+      return updatedProduct;
+    } catch (error) {
+      throw new Error('Error al actualizar el producto');
     }
+  }
 
+  async deleteProduct(pid) {
+    try {
+      const products = await this.getProducts();
+      const index = products.findIndex((p) => p.id === Number(pid));
 
-    async updateProduct(pid, updatedFields) {
-        try {
-            const products = await this.getProducts();
-            const index = products.findIndex((p) => p.id === parseInt(pid));
+      if (index === -1) {
+        throw new Error('El producto no existe');
+      }
 
-            if (index === -1) {
-                throw new Error('El producto no existe');
-            }
-
-            const updatedProduct = {
-                ...products[index],
-                ...updatedFields
-            };
-
-            products[index] = updatedProduct;
-            await fs.writeFile(this.filePath, JSON.stringify(products, null, '\t'));
-            return updatedProduct;
-        } catch (error) {
-            throw new Error('Error al actualizar el producto');
-        }
+      products.splice(index, 1);
+      await fs.writeFile(this.filePath, JSON.stringify(products, null, '\t'));
+    } catch (error) {
+      throw new Error('Error al eliminar el producto');
     }
-
-    async deleteProduct(pid) {
-        try {
-            const products = await this.getProducts();
-            const index = products.findIndex((p) => p.id === parseInt(pid));
-
-            if (index === -1) {
-                throw new Error('El producto no existe');
-            }
-
-            products.splice(index, 1);
-            await fs.writeFile(this.filePath, JSON.stringify(products, null, '\t'));
-        } catch (error) {
-            throw new Error('Error al eliminar el producto');
-        }
-    }
-
-
+  }
 }
-
 
 export default ProductManager;
