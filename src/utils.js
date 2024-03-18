c0a3fafa0c34aab5141b3d31e5541cd98df3ce98
Felipe Arce <arceprogramando@gmail.com>
2024-03-18 09:54:00 -0300

    style(Backend): :art: format code

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,8 @@
-
+/* eslint-disable no-underscore-dangle */
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-
-export default __dirname
+export default __dirname;
